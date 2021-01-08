b395218b560a9e172b4f435b10da08ead7fcf35d
SimonsPCC <73341402+SimonsPCC@users.noreply.github.com>
2021-01-08 23:52:55 +0100

    Add files via upload

---
+++ derdeJS/script.js
@@ -0,0 +1,24 @@
+//leeftijdcontrole
+var age = 18
+if (age > 17) {
+    console.log("Jij mag binnen komen")
+    
+}
+else { console.log("jij mag er niet in, buiten blijven aub")};
+//ladies night
+const isFemale = false;
+
+if (isFemale) {
+    console.log("Fijne avond mevrouw")
+}
+else {console.log("Jij mag er niet in, buiten blijven aub")};
+
+//veilig thuiskomen
+const driverStatus = "bop"
+if (driverStatus === "bob") { 
+    console.log("Jij bobt")
+    
+} else {
+    console.log("Jij hebt teveel gedronken")
+    
+};
